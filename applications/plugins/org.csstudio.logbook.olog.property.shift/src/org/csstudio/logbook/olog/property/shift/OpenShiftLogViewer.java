27d25f6caa6c8a24c026e8a3f605dee350c2623a
Kunal Shroff <Kunal Shroff@KunalShroff-PC>
2014-03-26 09:53:47 -0400

    OPEN - task 440: LogViewer application and Shift application integration http://github.com/ControlSystemStudio/cs-studio/issues/issue/440

--- applications/plugins/org.csstudio.logbook.olog.property.shift/src/org/csstudio/logbook/olog/property/shift/OpenShiftLogViewer.java
+++ applications/plugins/org.csstudio.logbook.olog.property.shift/src/org/csstudio/logbook/olog/property/shift/OpenShiftLogViewer.java
@@ -6,21 +6,24 @@ package org.csstudio.logbook.olog.property.shift;
 import gov.bnl.shiftClient.Shift;
 
 import org.csstudio.logbook.ui.LogTreeView;
-import org.csstudio.logbook.ui.LogViewerPerspective;
+import org.csstudio.ui.util.dialogs.ExceptionDetailsErrorDialog;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * @author shroffk
  * 
+ * Command to automatically search for log entries associated with the selected shift
+ * 
+ * The command opens if not already open the logTreeView with a query associated with the shift property.
  */
 public class OpenShiftLogViewer extends AbstractHandler {
 
@@ -32,9 +35,8 @@ public class OpenShiftLogViewer extends AbstractHandler {
     }
 
     @Override
-    public Object execute(ExecutionEvent event) throws ExecutionException {
+    public Object execute(final ExecutionEvent event) throws ExecutionException {
 	final IWorkbench workbench = PlatformUI.getWorkbench();
-	final IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
 	ISelection selection = HandlerUtil.getCurrentSelection(event);
 	if (selection instanceof IStructuredSelection) {
 	    IStructuredSelection strucSelection = (IStructuredSelection) selection;
@@ -49,13 +51,20 @@ public class OpenShiftLogViewer extends AbstractHandler {
 	    // TODO invalid selection
 	}
 	try {
-	    IWorkbenchPage page = workbench.showPerspective(LogViewerPerspective.ID, window);
-	    LogTreeView logTreeView = (LogTreeView)page.findView(LogTreeView.ID);
-	    page.showView(LogTreeView.ID);
-	    logTreeView.setSearchString(searchString);
-	} catch (Exception ex) {
-	    // ExceptionDetailsErrorDialog.openError(HandlerUtil.getActiveShell(event),
-	    // "Error executing command...", ex);
+		IWorkbenchPage page = workbench.getActiveWorkbenchWindow().getActivePage();
+	    LogTreeView logTreeView = (LogTreeView)page.showView(LogTreeView.ID);	   
+	    if(logTreeView != null){ 
+	    	logTreeView.setSearchString(searchString);
+	    }
+	} catch (final Exception ex) {
+		Display.getDefault().asyncExec(new Runnable() {
+			
+			@Override
+			public void run() {
+			     ExceptionDetailsErrorDialog.openError(HandlerUtil.getActiveShell(event),
+			     "Error executing command...", ex);
+			}
+		});
 	}
 	return null;
     }
